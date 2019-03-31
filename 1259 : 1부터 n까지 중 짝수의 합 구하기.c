459956e1cf64b8798b6758e188bc4a21045860da
sungyun5423 <47241272+sungyun5423@users.noreply.github.com>
2019-03-31 13:10:09 +0900

    Create 1259 : 1부터 n까지 중 짝수의 합 구하기.c

---
+++ 1259 : 1부터 n까지 중 짝수의 합 구하기.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	int i, a, sum = 0;
+	
+	scanf("%d", &a);
+	
+	for(i = 0; i <= a; i++)
+	{
+		if(i % 2 == 0)
+		{
+			sum += i;
+		}
+	}
+		
+	printf("%d", sum);
+}
