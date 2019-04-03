56851dc4774e860a6e11bfa9ed40bdb187d46e09
sungyun5423 <47241272+sungyun5423@users.noreply.github.com>
2019-04-03 20:48:49 +0900

    Create 1172 : 세 수 정렬하기.c

---
+++ 1172 : 세 수 정렬하기.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+	int a, b, c;
+	
+	scanf("%d %d %d", &a, &b, &c);
+	
+	if(a <= b && c <= b)
+	{
+		if(a < c)
+		{
+			printf("%d %d %d", a, c, b);
+		}
+		else
+			printf("%d %d %d", c, a, b);
+	}
+	
+	else if(b <= a && c <= a)
+	{
+		if(b < c)
+			printf("%d %d %d", b, c, a);
+		else
+			printf("%d %d %d", c, b, a);
+	}
+	
+	else if(a <= c && b <= c)
+	{
+		if(a < b)
+			printf("%d %d %d", a, b, c);
+		else
+			printf("%d %d %d", b, a, c);
+	}
+	
+	else if(a == b == c)
+		printf("%d %d %d", a, b, c);
+}
