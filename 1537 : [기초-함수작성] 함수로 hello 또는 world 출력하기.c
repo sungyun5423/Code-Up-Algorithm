3302c6c500b77b0580b74e338f2bf8e6e12fa29b
sungyun5423 <47241272+sungyun5423@users.noreply.github.com>
2019-04-06 22:53:04 +0900

    Create 1537 : [기초-함수작성] 함수로 hello 또는 world 출력하기.c

---
+++ 1537 : [기초-함수작성] 함수로 hello 또는 world 출력하기.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int n;
+
+
+
+void f(int x)
+{
+	if(x == 1) printf("hello\n");
+	else if(x == 2) printf("world\n");
+	else printf("\n");
+}
+int main()
+{
+  scanf("%d", &n);
+  f(n);
+  return 0;
+}
